1dc5bc34d641da80947a7d89a840d1d8fab47112
paul <paul.hildebrandt@tum.de>
2022-04-03 16:40:16 +0200

    QC pass 6

--- rename.py
+++ rename.py
@@ -1,10 +1,9 @@
 ## Rename numerically
-## has to be run outside of /tools
 
 import os
 seq = 0
 
 for file in os.listdir("imgs/"):
     full = "imgs/" + file
-    os.rename(full, "imgs/" + str(str('%04d' % seq) + "_" + str(file)))
+    os.rename(full, "imgs/" + str(str('%04d' % seq) + "_" + str(file)[6:]))
     seq +=1
