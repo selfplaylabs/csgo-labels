640b916f280258fcfc6dff2de8c81de7ef3ede35
paul <paul.hildebrandt@tum.de>
2022-04-07 13:12:44 +0200

    QC pass 9 | 2500-2250

--- tools/rename.py
+++ tools/rename.py
@@ -1,8 +1,6 @@
 ## Rename numerically
-
 import os
 seq = 0
-
 for file in os.listdir("imgs/"):
     full = "imgs/" + file
     os.rename(full, "imgs/" + str(str('%04d' % seq) + "_" + str(file)[6:]))
